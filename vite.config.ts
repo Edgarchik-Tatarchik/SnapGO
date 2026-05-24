53d82453fd7da25d0e9d67281fff1eb0ce08cbb7
Edgarchik-Tatarchik <atsw332@gmail.com>
2026-05-24 23:30:11 +0900

    add new app icon

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     display: 'standalone',
     orientation: 'portrait',
     icons: [
-      { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { src: '/icon-1337.png', sizes: '192x192', type: 'image/png' },
       { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
     ],
   },
